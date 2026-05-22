ecad0ea1813023b77529724cc6f0bfeb316acfc2
naims <naimdrive6@gmail.com>
2026-05-22 21:58:07 +0600

    feat: handle unhandled rejecttion and gracefully shutdown handler.

--- src/server.ts
+++ src/server.ts
@@ -22,6 +22,38 @@ const bootstrap = async () => {
 
     // start cron jobs
     startCronJobs();
+
+    // handle server shutdown gracefully
+    const exitHandler = () => {
+      if (server) {
+        server.close(() => {
+          console.log("Server closed gracefully.");
+          process.exit(1);
+        });
+      } else {
+        process.exit(1);
+      }
+    };
+
+    process.on("SIGINT", exitHandler);
+    process.on("SIGTERM", exitHandler);
+
+    // handle unhandled promise rejections
+    process.on("unhandledRejection", (error) => {
+      console.log(
+        "Unhandled Rejection is detected, we are closing our server...",
+      );
+      console.error("Error:", error);
+      console.error("Stack:", (error as Error).stack);
+      if (server) {
+        server.close(() => {
+          console.log(error);
+          process.exit(1);
+        });
+      } else {
+        process.exit(1);
+      }
+    });
   } catch (error) {
     console.log("Server Error", error);
     await prisma.$disconnect();
