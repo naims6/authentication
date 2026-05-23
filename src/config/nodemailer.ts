d9e58800becf0bb34b0e285c6a6f929a9da5f136
naims <naimdrive6@gmail.com>
2026-05-23 20:48:22 +0600

    fix: nodemailer production error.

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -4,21 +4,30 @@ import config from "./env.js";
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
+  secure: false,
   auth: {
     user: config.email_user,
     pass: config.email_pass,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 const sendMail = async (to: string, subject: string, html: string) => {
-  const mailOptions = {
-    from: config.email_user,
-    to,
-    subject,
-    html,
-  };
+  try {
+    const info = await transporter.sendMail({
+      from: config.email_user,
+      to,
+      subject,
+      html,
+    });
 
-  await transporter.sendMail(mailOptions);
+    console.log("EMAIL SENT:", info.response);
+  } catch (error) {
+    console.error("EMAIL ERROR:", error);
+    throw error;
+  }
 };
 
 export { sendMail };
